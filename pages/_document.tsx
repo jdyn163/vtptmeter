f930b7058f387af19fad356cac23e0aad82ebec6
Masie <jdyn163@gmail.com>
2026-01-08 02:42:38 -0800

    Restore room page history and original document meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,18 @@
-// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#111111" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
       <body>
         <Main />
